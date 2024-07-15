e378d7c12b02346fb24e3950037e8004006f16dd
Ofer Sadan <ofersadan85@gmail.com>
2024-07-16 00:55:18 +0300

    socket shutdown on every read

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::net::SocketAddr;
 use std::sync::Arc;
 use std::time::SystemTime;
-use tokio::fs::OpenOptions;
+use tokio::fs::{File, OpenOptions};
 use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader, BufWriter};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::Mutex;
 
 const PORT: u16 = 8001;
@@ -13,6 +14,44 @@ fn now() -> u128 {
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap_or_default()
         .as_millis()
+}
+
+async fn log_message(
+    file: Arc<Mutex<BufWriter<File>>>,
+    socket: &mut TcpStream,
+    client: &SocketAddr,
+    bytes_counter: Arc<Mutex<u64>>,
+) -> std::io::Result<()> {
+    let mut reader = BufReader::new(socket);
+    let mut buffer = vec![0; 4096];
+    let n = match reader.read(&mut buffer).await {
+        Ok(n) if n > 0 => n,
+        Err(_) | Ok(_) => {
+            file.lock()
+                .await
+                .flush()
+                .await
+                .expect("Failed to flush file");
+            return Ok(());
+        }
+    };
+    let line_stamp = format!("$$${}$$${}$$${n}$$$", now(), client);
+    {
+        let mut file_guard = file.lock().await;
+        file_guard.write_all(line_stamp.as_bytes()).await?;
+        file_guard.write_all(&buffer[0..n]).await?;
+        file_guard.flush().await?;
+    } // file_guard goes out of scope and releases the lock
+    {
+        let mut bytes_guard = bytes_counter.lock().await;
+        if *bytes_guard > MAX_FILE_SIZE {
+            eprintln!("File size exceeds the limit of {MAX_FILE_SIZE} bytes");
+            std::process::exit(1);
+        } else {
+            *bytes_guard += n as u64;
+        }
+    } // bytes_guard goes out of scope and releases the lock
+    Ok(())
 }
 
 #[tokio::main]
@@ -26,68 +65,28 @@ async fn main() -> std::io::Result<()> {
         .write(true)
         .append(true)
         .open(file_path)
-        .await
-        .expect("Failed to open file");
-    let previous_bytes_written = raw_file
-        .metadata()
-        .await
-        .expect("Failed to read file length")
-        .len();
+        .await?;
+    let previous_bytes_written = raw_file.metadata().await?.len();
     if previous_bytes_written > MAX_FILE_SIZE {
-        eprintln!("File size exceeds the limit of {MAX_FILE_SIZE} bytes");
-        std::process::exit(1);
+        let message = format!("File size exceeds the limit of {MAX_FILE_SIZE} bytes");
+        return Err(std::io::Error::new(std::io::ErrorKind::Other, message));
     }
     let bytes_counter = Arc::new(Mutex::new(previous_bytes_written));
     let file = Arc::new(Mutex::new(BufWriter::new(raw_file)));
 
     loop {
-        let (socket, client) = listener.accept().await?;
-        let file_clone = Arc::clone(&file);
+        let file = Arc::clone(&file);
+        let (mut socket, client) = listener.accept().await?;
         let bytes_counter = Arc::clone(&bytes_counter);
         tokio::spawn(async move {
-            let mut reader = BufReader::new(socket);
-            let mut buffer = vec![0; 4096];
-            let mut write_counter = 0;
-            loop {
-                let n = match reader.read(&mut buffer).await {
-                    Ok(n) if n > 0 => n,
-                    Err(_) | Ok(_) => {
-                        file_clone
-                            .lock()
-                            .await
-                            .flush()
-                            .await
-                            .expect("Failed to flush file");
-                        break;
-                    }
-                };
-                let line_stamp = format!("$$${}$$${}$$${n}$$$", now(), client);
-                {
-                    let mut file_guard = file_clone.lock().await;
-                    file_guard
-                        .write_all(line_stamp.as_bytes())
-                        .await
-                        .expect("Failed to write to file");
-                    file_guard
-                        .write_all(&buffer[0..n])
-                        .await
-                        .expect("Failed to write to file");
-                    write_counter += n;
-                    if write_counter >= 4096 {
-                        file_guard.flush().await.expect("Failed to flush file");
-                        write_counter = 0;
-                    }
-                } // file_guard goes out of scope and releases the lock
-                {
-                    let mut bytes_guard = bytes_counter.lock().await;
-                    if *bytes_guard > MAX_FILE_SIZE {
-                        eprintln!("File size exceeds the limit of {MAX_FILE_SIZE} bytes");
-                        std::process::exit(1);
-                    } else {
-                        *bytes_guard += n as u64;
-                    }
-                } // bytes_guard goes out of scope and releases the lock
-            }
+            log_message(file, &mut socket, &client, bytes_counter)
+                .await
+                .unwrap_or_else(|e| {
+                    eprintln!("Failed to log message from {client}: {e}");
+                });
+            socket.shutdown().await.unwrap_or_else(|e| {
+                eprintln!("Failed to shutdown client socket {client}: {e}");
+            });
         });
     }
 }
